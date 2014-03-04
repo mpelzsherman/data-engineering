f63fe4831a8bc43ac25b3fa7e53e7a2b2d5b6dcd
mpelzsherman <mpelzsherman@gmail.com>
2014-03-03 22:40:46 -0500

    add required arg

--- tallyman/test/lib/tallyman/purchase_file_parser_test.rb
+++ tallyman/test/lib/tallyman/purchase_file_parser_test.rb
@@ -7,7 +7,7 @@ class PurchaseFileParserTest < ActionController::TestCase
   test 'should create purchases' do
     assert_difference('Purchase.count', 4) do
       pfp = PurchaseFileParser.new
-      pfp.create_purchases(File.join(Rails.root, 'test/fixtures', 'example_input.tab'))
+      pfp.create_purchases(File.join(Rails.root, 'test/fixtures', 'example_input.tab'), nil)
     end
   end
 
